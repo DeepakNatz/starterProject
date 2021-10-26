51fd3c482ee11bd19c81e199b462d9aebe1cc8e8
DpkNatz <deepaknatarajan22@gmail.com>
2021-10-26 13:35:02 +0530

    updated master

--- script/script.js
+++ script/script.js
@@ -3,11 +3,11 @@ const myImage = document.querySelector("img");
 myImage.onclick = function(){
     let mySrc = myImage.getAttribute("src");
 
-    if(mySrc === "/images/firefox-icon.png"){
-        myImage.setAttribute('src','/images/firefox2.png');
+    if(mySrc === "images/firefox-icon.png"){
+        myImage.setAttribute('src','images/firefox2.png');
     }
-    else if(mySrc === "/images/firefox2.png"){
-        myImage.setAttribute("src","/images/firefox-icon.png");
+    else if(mySrc === "images/firefox2.png"){
+        myImage.setAttribute("src","images/firefox-icon.png");
     }
 }
 const Button = document.querySelector("button");
